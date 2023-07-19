cc56d692877ff165d97c25b8e0d9ce9f2bb120b3
Adambasha <133229688+imadambhai@users.noreply.github.com>
2023-07-19 14:04:53 +0530

    MyPrograms

--- My_Programs/src/PatternPrograms/Pattern1.java
+++ My_Programs/src/PatternPrograms/Pattern1.java
@@ -9,6 +9,11 @@ public class Pattern1 {
 //	3 4 5 
 //	2 3 4 5 
 //	1 2 3 4 5 
+//	5 
+//	4 5 
+//	3 4 5 
+//	2 3 4 5 
+//	1 2 3 4 5 
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
